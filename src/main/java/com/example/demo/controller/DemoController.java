a6f9150d6e52b5c563fafd2db1f7d0487674c462
viswa <viswavard>
2024-09-02 11:41:20 +0530

    added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo/api")
 public class DemoController {
 	@GetMapping ("/welcome")
-	public String getMethodName() {
+	public String welcome() {
 		return "heyy";
 	}
 
